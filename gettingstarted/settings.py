3a8df9c3bcab9980b161d338399aff0e4d7f0379
vivianmahr <vivianmahr@gmail.com>
2018-07-04 16:38:29 -0700

    databse setup

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import config
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -76,13 +77,23 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vivipan_info',
+        'USER': 'postgres',
+        'PASSWORD': config.POSTGRES_PW,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
